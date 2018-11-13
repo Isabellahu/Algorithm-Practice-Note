ab2ed01ca4bf6047d66732e1849d9d6b4fd50318
姬香 <35736172+Isabellahu@users.noreply.github.com>
2018-11-13 14:50:06 +0800

    Update 3-4 leetcode-27.py

--- 3-4 leetcode-27.py
+++ 3-4 leetcode-27.py
@@ -15,3 +15,21 @@ class Solution:
 
         nums = nums[:k]
         return len(nums)
+
+    
+# 26. Remove Duplicates from Sorted Array
+
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k=0
+        for i in range(len(nums)-1):
+            if nums[i] != nums[i+1]:
+
+                nums[k+1] = nums[i+1]
+                k+=1
+        nums = nums[:k+1]
+        return len(nums)
