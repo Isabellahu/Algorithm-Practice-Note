b87776dcbb5d7746f2d40c4121abedd876c5f975
姬香 <35736172+Isabellahu@users.noreply.github.com>
2018-12-06 00:03:39 +0800

    Update 3-6 对撞指针 167|125|344|345|11.py

--- 3-6 对撞指针 167|125|344|345|11.py
+++ 3-6 对撞指针 167|125|344|345|11.py
@@ -16,3 +16,40 @@ while l<r:
         l+=1
     else:
         r-=1
+
+        
+        
+# leetcode-125
+# 只看数字和字母， 不考虑大小写
+# 空字符串的处理
+
+s = "A man, a plan, a canal: Panama"
+ssd = "race a car"
+
+# Python isalnum() 方法检测字符串是否由字母和数字组成
+new_s = [i for i in s.upper() if i.isalnum()]
+error = 0
+l = 0
+r = len(new_s)-1
+       
+while l<r:
+    if new_s[l] != new_s[r]:
+        error = 1        
+    l+=1
+    r-=1 
+
+print(error==0)
+
+
+##method2
+# 直接比较原字符串与回文序列
+s = "A man, a plan, a canal: Panama"
+s = "race a car"
+
+# Python isalnum() 方法检测字符串是否由字母和数字组成
+new_s = [i for i in s.upper() if i.isalnum()]
+# a[i:j:s]  s表示步进，缺省为1
+# a[i:j:1] == a[i:j]
+# a[::-1] 回文序列
+print(new_s == new_s[::-1])
+
