fc3d833a0992b5b00895f2325c85f7e36907c2cf
姬香 <35736172+Isabellahu@users.noreply.github.com>
2018-12-14 00:07:04 +0800

    Update 3-8 滑动窗口-3|438|76.py

--- 3-8 滑动窗口-3|438|76.py
+++ 3-8 滑动窗口-3|438|76.py
@@ -77,3 +77,53 @@ else:
 print(res)
 
 
+# leetcode-76
+'''
+通过两个数组分别记录目标字符串每个字母出现次数 和 记录窗口内每个字母出现次数
+如果窗口数组中的字母对应次数 <= 目标数组中的字母对应次数， 则完成一次对应
+当符合要求的字母全部对应，让左边界不断累加直到指向符合要求的数组，最短长度不断更迭
+'''
+s = "ABC"
+
+t = "ABC"
+
+min_length = len(s)
+num = 0
+
+if s == '' or t == '' or len(s) < len(t):
+    res = ""
+
+else:
+    res = ""
+    # 记录目标字符串每个字母出现次数
+    t_char = [0] * 128
+    for i in range(len(t)):
+        t_char[ord(t[i])] += 1
+
+    # 用于记录窗口内每个字母出现次数
+    window_char = [0] * 128
+
+    l = 0
+
+    for r in range(len(s)):
+        window_char[ord(s[r])] += 1
+        # 如果窗口数组中的字母对应次数 <= 目标数组中的字母对应次数， 则完成一次对应
+        if t_char[ord(s[r])] >= window_char[ord(s[r])]:
+            num += 1
+        # 当符合要求的字母全部对应，，
+        if num == len(t):
+            # 让左边界不断累加直到指向符合要求的数组
+            while l < r and t_char[ord(s[l])] < window_char[ord(s[l])]:
+                window_char[ord(s[l])] -= 1
+                l += 1
+            # 最短长度不断更迭
+            if r - l < min_length:
+                min_length = r - l
+                res = s[l:r+1]
+                print(res)
+            # 把开头的这个匹配字符跳过，并将匹配字符数减1
+            window_char[ord(s[l])] -= 1
+            num -= 1
+            l += 1
+
+print('res = ', res)
