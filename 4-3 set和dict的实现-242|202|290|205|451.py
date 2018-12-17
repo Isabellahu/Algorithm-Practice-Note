eb295a6b7283238bd53008efd0c7812814774a51
姬香 <35736172+Isabellahu@users.noreply.github.com>
2018-12-17 17:52:31 +0800

    Update 4-3 set和dict的实现-242|202|290|205|451.py

--- 4-3 set和dict的实现-242|202|290|205|451.py
+++ 4-3 set和dict的实现-242|202|290|205|451.py
@@ -20,3 +20,26 @@ for i in t:
 print(dict2 == dict1)
 
 # leetcode-202
+# set 记录出现的所有数字，以防重复计算
+data = set()
+
+while True:
+    print(n)
+    # sum 清零
+    sum_v = 0
+    while n > 0:
+        sum_v += (n%10)*(n%10)
+        n = n //10
+    n = sum_v 
+    if n == 1:
+        return True
+    if n in data:
+        return False
+    data.add(n)
+    
+    
+# leetcode-290
+
+# leetcode-205
+
+# leetcode-451
