bb35e8739ce8e2b7fc21dd5330aebe22f7bcf22b
姬香 <35736172+Isabellahu@users.noreply.github.com>
2018-12-26 15:41:23 +0800

    Update 4-4 查找表-1|15|18|16.py

--- 4-4 查找表-1|15|18|16.py
+++ 4-4 查找表-1|15|18|16.py
@@ -54,5 +54,68 @@ for i in range(len(nums)-2):
 print(res)
 
 
+
 # leetcode-18
+nums = [-1,0,1,2,-1,-4]
+target = -1
+
+nums.sort()
+print(nums)
+res = []
+for i in range(len(nums)-3):
+    # 第一位数字：防止重复
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+    for j in range(i+1, len(nums)-2):
+        # 第二位数字：防止选择了相同元素，但可以和第一位数字相同
+        if j > i+1 and nums[j] == nums[j-1]:
+            continue
+        l = j + 1
+        r = len(nums) - 1
+        while l < r:
+            if nums[l] + nums[r] + nums[i] + nums[j] == target:
+                res.append([nums[i], nums[j], nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+                while l < r and nums[r] == nums[r+1]:
+                    r -= 1
+
+            elif nums[l] + nums[r] + nums[i] + nums[j] < target:
+                l += 1
+            else:
+                r -= 1
+
+print(res)
+
+
+
 # leetcode-16
+nums = [0,0,1]
+target = 1
+
+import sys
+nums.sort()
+print(nums)
+# 距离对比值
+dist = sys.maxsize
+
+for i in range(len(nums)-2):
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+    l, r = i + 1, len(nums) -1
+
+    while l < r:
+        three_sum = nums[i] + nums[l] + nums[r]
+        if three_sum == target:
+            return three_sum
+        elif three_sum < target:
+            l += 1
+        else:
+            r -= 1
+        # 不断迭代最近距离的三数和
+        if abs(three_sum - target) < abs(dist - target):
+            dist = three_sum
+return dist
+
