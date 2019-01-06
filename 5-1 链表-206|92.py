a8e0b2562897af920ac0baf7c24706bde51c3773
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-06 22:26:31 +0800

    Update 5-1 链表-206|92.py

--- 5-1 链表-206|92.py
+++ 5-1 链表-206|92.py
@@ -23,6 +23,31 @@ class Solution:
             pre = cur
             cur = nex
         return pre
-        
+    
+# 调试程序——version
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+a.next = b
+b.next = c
+c.next = d
+d.next = ListNode(None)
+head = a
+####
+
+pre = None
+cur = head
+while cur:
+    nex = cur.next
+    cur.next = pre
+    pre = cur
+    cur = nex
+print(pre.val)
+
                 
 # leetcode - 92. Reverse Linked List II
