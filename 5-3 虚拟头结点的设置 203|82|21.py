85bddff3e945aade4f6fc4c1578b75e3f05006ce
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-16 17:06:45 +0800

    Create 5-3 虚拟头结点的设置 203|82|21.py

---
+++ 5-3 虚拟头结点的设置 203|82|21.py
@@ -0,0 +1,36 @@
+# Leetcode - 203
+
+# # method 1
+# 如果头结点不为空且头结点的值为目标值
+while head is not None and head.val == val:
+    head = head.next
+# 如果循环之后头结点为空
+if head is None:
+    return head
+# 循环遍历头结点之后的结点
+cur = head
+while cur.next:
+    if cur.next.val == val:
+        delNode = cur.next
+        cur.next = delNode.next
+    else:
+        cur = cur.next
+return head
+
+# # method 2
+# 创建虚拟头结点
+dummyNode = ListNode(0)
+dummyNode.next = head
+cur = dummyNode
+while cur.next:
+    if cur.next.val == val:
+        delNode = cur.next
+        cur.next = delNode.next
+    else:
+        cur = cur.next
+return dummyNode.next
+
+
+# Leetcode - 82
+
+# Leetcode - 21
