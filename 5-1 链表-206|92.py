ab5cd8a94a2593d52fb227063781edee3fcc40a1
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-17 10:54:58 +0800

    Update 5-1 链表-206|92.py

--- 5-1 链表-206|92.py
+++ 5-1 链表-206|92.py
@@ -1,10 +1,10 @@
-# leetcode - 206. Reverse Linked List
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+# leetcode - 206. Reverse Linked List
 class Solution:
     def reverseList(self, head):
         """
