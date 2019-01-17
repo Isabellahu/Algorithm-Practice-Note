0b39d13e6833e1bb79d9cf7cc70d1687e9c26b12
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-17 22:52:11 +0800

    Update 5-4 24|25|147|148.py

--- 5-4 24|25|147|148.py
+++ 5-4 24|25|147|148.py
@@ -16,7 +16,39 @@ while node1 and node1.next:
 return dummy_node.next
 
 
-# Leetcode - 25
+# Leetcode - 25. Reverse Nodes in k-Group
+def reverse_list(head):
+    cur = head
+    pre = None
+    while cur:
+        nex = cur.next
+        cur.next = pre
+        pre = cur
+        cur = nex
+    return pre
+
+# 使用三个链表：原始链表， 反转链表， 结果链表
+res = ListNode(0)
+l3 = res
+
+while head:
+    i = 0
+    dummy_node = ListNode(0)
+    l2 = dummy_node
+    while i < k and head:
+        l2.next = ListNode(head.val)
+        l2 = l2.next
+        head = head.next
+        i += 1
+    if i == k:
+        res.next = reverse_list(dummy_node.next)
+    else:
+        res.next = dummy_node.next
+
+    while res.next:
+        res = res.next
+return l3.next
+
 
 # Leetcode - 147
 
