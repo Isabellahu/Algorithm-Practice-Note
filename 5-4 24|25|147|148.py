d94e9e452c350d392b9248dacca85e7e67a913ce
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-17 14:26:43 +0800

    Update 5-4 24|25|147|148.py

--- 5-4 24|25|147|148.py
+++ 5-4 24|25|147|148.py
@@ -1,4 +1,20 @@
 # Leetcode - 24
+# 设置虚拟头结点
+dummy_node = ListNode(0)
+dummy_node.next = head
+# node1 node2 表示交换的两个结点
+node1 = dummy_node.next
+# pre需要指向node2而不是node1
+pre = dummy_node
+while node1 and node1.next:
+    node2 = node1.next
+    node1.next = node2.next
+    node2.next = node1
+    pre.next = node2
+    pre = node1
+    node1 = node1.next
+return dummy_node.next
+
 
 # Leetcode - 25
 
