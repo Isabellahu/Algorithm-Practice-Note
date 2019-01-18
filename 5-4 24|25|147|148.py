25f0bba2efe4689f59933bedef8421041714f98a
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-01-18 12:23:07 +0800

    Update 5-4 24|25|147|148.py

--- 5-4 24|25|147|148.py
+++ 5-4 24|25|147|148.py
@@ -17,6 +17,7 @@ return dummy_node.next
 
 
 # Leetcode - 25. Reverse Nodes in k-Group
+# method 1
 def reverse_list(head):
     cur = head
     pre = None
@@ -48,6 +49,45 @@ while head:
     while res.next:
         res = res.next
 return l3.next
+
+# method 2
+def PrintList(curNode):
+    while curNode is not None:
+        print(curNode.val, '-> ', end='')
+        curNode = curNode.next
+    print('null', '\n')
+# 设置虚拟头结点
+res = ListNode(0)
+res.next = head
+# pre为虚拟头结点，cur为head
+pre = res
+cur = head
+temp = head
+i = 0
+# i为所有节点的总个数
+while temp:
+    i += 1
+    temp = temp.next
+# 分组翻转
+# 0-1-2-3-4 变成 0-2-1-3-4 再变成 0-3-2-1-4 ，  k=3
+while i >= k:
+    # 反转链表的操作，执行（k-1）次
+    for j in range(k-1):
+        # 之后修改链接无法找到 pre.next
+        node = pre.next
+        # pre.next 需要 cur.next
+        pre.next = cur.next
+        # cur.next 需要 pre.next.next
+        cur.next = pre.next.next
+        # pre.next找到之后再次next 到node
+        pre.next.next = node
+    # print(pre.val)
+    # print(cur.val)
+    pre = cur
+    cur = cur.next
+    # i个结点减去已经反转过的k个结点得到剩余节点个数
+    i -= k
+PrintList(res)
 
 
 # Leetcode - 147
