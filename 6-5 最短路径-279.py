eba9829d621c8731a724265ef69ab2faacc88867
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-06-11 18:24:59 +0800

    Update 6-5 最短路径-279.py

--- 6-5 最短路径-279.py
+++ 6-5 最短路径-279.py
@@ -16,3 +16,45 @@ public:
 };
 
 # method 2
+class Solution {
+public:
+    int numSquares(int n) {
+        queue<pair<int, int>> q;
+        q.push( make_pair(n, 0) );//从n出发，到达n需要0步
+        
+        vector<bool> visited(n+1, false);
+        visited[n] = true;
+        while(!q.empty()){
+            int num = q.front().first; //节点
+            int step = q.front().second; //步数
+
+            q.pop();
+
+            if (num == 0)
+                return step;
+            
+            for(int i = 1; ; i++){
+                int a = num - i *i;
+                if( a < 0 )
+                    break;
+                # 没有计算过的节点，以防重复
+                if( visited[a]== false ){
+                    q.push( make_pair(a, step + 1) );
+                    visited[a] = true;
+                    
+                }
+                
+            }
+                
+        }
+        # 抛出invalid_argument类异常对象
+        throw invalid_argument("no answer!");
+    }
+
+            
+};
+
+
+    
+
+    
