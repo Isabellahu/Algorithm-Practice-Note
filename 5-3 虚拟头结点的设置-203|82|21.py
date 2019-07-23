9d35b9c0e8884d6e8de61249726a970d1ae41f77
姬香 <35736172+Isabellahu@users.noreply.github.com>
2019-07-23 17:05:16 +0800

    Update 5-3 虚拟头结点的设置-203|82|21.py

--- 5-3 虚拟头结点的设置-203|82|21.py
+++ 5-3 虚拟头结点的设置-203|82|21.py
@@ -56,13 +56,17 @@ return dummy_node.next
 
 
 # Leetcode - 21
-
-
-
-
-
-
-
-
-
-
+# 递归的方式，返回链表１和２中指针指向的较小的值
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        if l1.val <= l2.val:
+            head = l1
+            head.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            head = l2
+            head.next = self.mergeTwoLists(l1, l2.next)
+        return head
